2613f704e0a7c855a64aa66f8be54343f9102fad
Heder Alves <alvesheder92@gmail.com>
2022-11-07 21:58:27 -0300

    Upload

---
+++ dist/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let result = document.getElementById('result');
+let x = 10;
+let y = 30;
+let xy = x + y;
+console.log(xy);
+const user = {
+    name: "heder",
+    password: 12345,
+    bead: ["personagem", 1, "Ranger", true]
+};
+console.log(user);
