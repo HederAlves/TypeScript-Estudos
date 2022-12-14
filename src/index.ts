b3bab65851fcd36e150be1b7cfb8cbb2db1d6ee8
Heder Alves <alvesheder92@gmail.com>
2022-12-13 21:39:01 -0300

    Update

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let result = document.getElementById('result');
+
+let x: number = 10;
+let y: number = 30;
+
+let xy: number = x + y;
+
+console.log(xy);
+
+
+const user: {name: string, password: number, bead: [string, number, string, boolean]} = {
+    name: "heder",
+    password: 12345,
+    bead:["personagem", 1, "Ranger", true]
+}
+
+console.log(user);
